d5cc3e3fc78397bd475fc9c76cc226fe0e6184c8
Nishant <nishantnehra0@gmail.com>
2025-09-06 17:55:19 +0530

    TMDB integration and main page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,20 @@
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import usePopularMovies from "../hooks/usePopularMovies";
 import Header from "./Header";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
-    return <Header />;
+    useNowPlayingMovies();
+    usePopularMovies();
+
+    return (
+        <>
+            <Header />
+            <MainContainer />
+            <SecondaryContainer />
+        </>
+    );
 };
 
 export default Browse;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,14 @@ export const LOGO =
 
 export const USER_AVATAR =
     "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS = {
+    method: "GET",
+    headers: {
+        accept: "application/json",
+        Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzRlYjdmMmNlOWZmYzU1YWJjMjFhOTc0MmY1ZjFhMiIsIm5iZiI6MTc1NzE0MTA0NS40ODgsInN1YiI6IjY4YmJkODM1NGIwZTgxNzkyODhjZTZkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dVcKpNWw43FpznFpRcMHpnAj-wwXKokva_1k06nNFks",
+    },
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
