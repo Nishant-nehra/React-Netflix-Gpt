2c8cf95c3eced3fb6c0a37bdea23794f43e63b3e
Nishant <nishantnehra0@gmail.com>
2025-09-07 13:15:38 +0530

    added open ai gpt func

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,9 +8,19 @@ export const API_OPTIONS = {
     method: "GET",
     headers: {
         accept: "application/json",
-        Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzRlYjdmMmNlOWZmYzU1YWJjMjFhOTc0MmY1ZjFhMiIsIm5iZiI6MTc1NzE0MTA0NS40ODgsInN1YiI6IjY4YmJkODM1NGIwZTgxNzkyODhjZTZkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dVcKpNWw43FpznFpRcMHpnAj-wwXKokva_1k06nNFks",
+        Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
     },
 };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
+export const BG_URL =
+    "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const SUPPORTED_LANGUAGES = [
+    { identifier: "en", name: "English" },
+    { identifier: "hindi", name: "Hindi" },
+    { identifier: "spanish", name: "Spanish" },
+];
+
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
